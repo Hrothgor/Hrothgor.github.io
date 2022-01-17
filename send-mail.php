11b4401ae3fe8ac9be3e7a9ee4f82585cd81ef81
Hrothgor <jul.minguet@gmail.com>
2022-01-17 17:50:44 +0100

    last projetc (my rpg) + CV and cleaning

--- send-mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $to = "jul.minguet@epitech.eu";
-    $message = wordwrap("test", 70, "\r\n");
-    $headers = array(
-        'From' => $from,
-    );
-    mail($to, $subject, $message, $headers);
-?>
